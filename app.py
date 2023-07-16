7cec01ece26d64ea3d54b73964b77777e19e0022
shayangit20 <m.shayanwaqar.03@gmail.com>
2023-07-16 16:33:49 +0500

    Add templates, inheritance, and bootstrap Extend templates from base.html add bootstrap and css/js to base.html for styling create template and static folders for html and css files respectively

--- app.py
+++ app.py
@@ -1,14 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author':'Corey Schafer',
+        'title':'blog post 1',
+        'content': 'first post content',
+        'date_posted':'April 20, 2018'
+    },
+    {
+        'author':'Shayan Waqar',
+        'title':'blog post 2',
+        'content':'second post content',
+        'date_posted':'July 16, 2023'
+    }
+]
+
+'''
+FLASK
+route	Flask route
+frt	    Render template
+routegp	Flask route with GET, POST
+routep	Flask route POST only
+
+JINJA2
+fvar    {{Name var}}
+ffor	Flask for
+fif	    Flask if
+fife	Flask ife
+felif	Flask elif
+'''
+
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title='title')
 
 if __name__ == '__main__':
     app.run(debug=True)
